26ab32a83fa7aa06abe411ee733907115cfa4913
Olga <ola.19@hotmail.com>
2019-08-13 11:14:57 +0200

    Wizard Invoice

--- src/retrieve_09S2p000001tD76EAE/aura/AccountWizardCmp/AccountWizardCmpHelper.js
+++
@@ -1,8 +0,0 @@
-({
-	handleEvent : function(component,event) {
-		var appEvent = $A.get("e.c:InvoiceWizardEvent");                
-        		appEvent.setParam("Step", "2");
-                appEvent.setParam("Account",component.get("v.accountId"));
-                appEvent.fire();
-	}
-})
